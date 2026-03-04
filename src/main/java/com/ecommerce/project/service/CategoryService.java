79b18e12ddc92e8ea403b57a2b23f6398d28dad3
Sahel <sahelstha2061@gmail.com>
2026-03-04 16:33:08 +0545

    added sorting by field name and order, and also added the default value in AppConstants class

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -7,7 +7,7 @@ import com.ecommerce.project.payload.CategoryResponse;
 import java.util.List;
 
 public interface CategoryService {
-    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize);
+    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize,  String sortBy, String sortOrder);
     CategoryDTO createCategory(CategoryDTO category);
     CategoryDTO deleteCategory(Long categoryId);
     CategoryDTO updateCategory(CategoryDTO categoryDto, Long categoryId);
