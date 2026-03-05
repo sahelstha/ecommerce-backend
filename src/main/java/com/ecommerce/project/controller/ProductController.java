3bdb59dfc0ceb3b64772efeb59ec71bf535a19f9
Sahel <sahelstha2061@gmail.com>
2026-03-05 19:35:32 +0545

    adding update image logic and updateImage api created

--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -52,5 +54,13 @@ public class ProductController {
     public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId) {
         ProductDTO deleteProductDto =  productService.deleteProduct(productId);
         return new ResponseEntity<>(deleteProductDto, HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/products/{productId}/image")
+    public ResponseEntity<ProductDTO> updateProductImage (@PathVariable Long productId,
+                                                         @RequestParam("image") MultipartFile image) throws IOException {
+        ProductDTO updatedProduct = productService.updateProductImage(productId, image);
+
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
     }
 }
--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -3,6 +3,9 @@ package com.ecommerce.project.service;
 import com.ecommerce.project.model.Product;
 import com.ecommerce.project.payload.ProductDTO;
 import com.ecommerce.project.payload.ProductResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface ProductService {
 
@@ -17,4 +20,6 @@ public interface ProductService {
     ProductDTO updateProduct(Long productId, ProductDTO productDto);
 
     ProductDTO deleteProduct(Long productId);
+
+    ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException;
 }
