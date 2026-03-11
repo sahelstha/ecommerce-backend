f6eb786f8370d747c97941a03448071fb0a08dac
Sahel <sahelstha2061@gmail.com>
2026-03-11 06:04:15 +0545

    add: CartItem and Cart Repository

--- src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
@@ -2,6 +2,9 @@ package com.ecommerce.project.repositories;
 
 import com.ecommerce.project.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    @Query("select ci from CartItem ci where ci.cart.cartId = ?1 and ci.product.productId = ?2")
+    CartItem findCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
--- src/main/java/com/ecommerce/project/repositories/CartRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CartRepository.java
@@ -1,7 +1,11 @@
 package com.ecommerce.project.repositories;
 
+import com.ecommerce.project.model.Cart;
 import com.ecommerce.project.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface CartRepository extends JpaRepository<CartItem, Long> {
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    @Query("SELECT c FROM Cart c WHERE c.user.email=?1")
+    Cart findCartByEmail(String email);
 }
